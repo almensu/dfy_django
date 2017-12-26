6d9dd54fd88248f279e04288c36856926630ac9d
almensu <yanghoo205@hotmail.com>
2017-12-26 13:40:46 +0800

    first dfy_django

---
+++ blog/views.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from . import models
+# Create your views here.
+
+
+# def index(request):
+#     return HttpResponse('hello world!dfy')
+
+def index(request):
+    # article = models.Article.objects.get(pk=1)
+    articles = models.Article.objects.all()
+    return render(request, 'blog/index.html', {'articles': articles})
+
+def article_page(request,article_id):
+    article = models.Article.objects.get(pk=article_id)
+    return render(request, 'blog/article_page.html', {'article': article})
