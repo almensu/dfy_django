edbc81d84919afa96e86137ca4a836b72f9529c3
almensu <yanghoo205@hotmail.com>
2017-12-27 17:50:27 +0800

    article_edit_page_action

--- blog/views.py
+++ blog/views.py
@@ -4,9 +4,10 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 from . import models
+from django.template import RequestContext
+
+
 # Create your views here.
-
-
 
 
 def index(request):
@@ -14,9 +15,21 @@ def index(request):
     articles = models.Article.objects.all()
     return render(request, 'blog/index.html', {'articles': articles})
 
+
 def article_page(request, article_id):
     article = models.Article.objects.get(pk=article_id)
     return render(request, 'blog/article_page.html', {'article': article})
 
+
 def article_edit_page(request):
     return render(request, 'blog/article_edit_page.html')
+
+
+def article_edit_page_action(request):
+    title = request.POST.get('title', '默认标题')
+    content = request.POST.get('content', '默认内容')
+    models.Article.objects.create(title=title, content=content)
+    articles = models.Article.objects.all()
+    return render(request, 'blog/index.html', {'articles': articles})
+
+    pass
